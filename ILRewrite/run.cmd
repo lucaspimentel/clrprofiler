ccc960f74deb9c3a608f6f2783098c98d242bc75
Lucas Pimentel <lucaspimentel@gmail.com>
2018-05-15 16:12:05 -0400

    add batch file to start profiling easily

---
+++ ILRewrite/run.cmd
@@ -0,0 +1,5 @@
+SET COR_ENABLE_PROFILING=1
+SET COR_PROFILER={FA8F1DFF-0B62-4F84-887F-ECAC69A65DD3}
+SET COR_PROFILER_PATH=C:\Users\lucas\source\repos\clrprofiler\ILRewrite\x64\Debug\ILRewriteProfiler.dll
+
+%1
